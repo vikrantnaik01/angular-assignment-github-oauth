387ba47c70f213abfdb077f273168e9cb2a84e73
Naik, Vikrant <vikrantnaik01@gmail.com>
2021-05-31 15:51:16 +0530

    Adding reusable component via lib and auth guard, Interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { OAuthModule } from 'angular-oauth2-oidc';
@@ -12,7 +12,8 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProfilePageComponent } from './components/profile-page/profile-page.component';
 import { ErrorComponent } from './components/error/error.component';
-// import { ProfileDataModule } from 'projects/profile-data/src/lib/profile-data.module';
+import { AuthInterceptor } from './services/auth-interceptor.service';
+import { ProfileDataModule } from 'projects/profile-data/src/lib/profile-data.module';
 
 @NgModule({
   declarations: [
@@ -25,12 +26,22 @@ import { ErrorComponent } from './components/error/error.component';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    OAuthModule.forRoot(),
+    OAuthModule.forRoot(
+      { resourceServer: {
+          sendAccessToken: true
+    }}),
     HttpClientModule,
     FormsModule,
-    // ProfileDataModule
+    ProfileDataModule
   ],
-  providers: [],
+  providers: [
+    // Used for access token, but because of new issue in OAuth with Github, commented this code.
+    // {
+    //   provide : HTTP_INTERCEPTORS,
+    //   useClass: AuthInterceptor,
+    //   multi   : true,
+    // },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
